67a39d3baca7e7bdb8ae3da86f4b5ef2112a5d6a
Chris Smith <chris.smith@careerbuilder.com>
2017-02-24 18:33:10 -0500

    initial commit

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject malone "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [metosin/compojure-api "1.1.10"]]
+  :ring {:handler malone.handler/app}
+  :uberjar-name "server.jar"
+  :profiles {:dev {:dependencies [[javax.servlet/javax.servlet-api "3.1.0"]]
+                   :plugins [[lein-ring "0.10.0"]]}})
