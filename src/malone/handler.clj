be5dc573577323733af4e620df9368f473d638ad
Chris Smith <chris.smith@careerbuilder.com>
2017-02-24 22:18:46 -0500

    Holy fuck that took forever

--- src/malone/handler.clj
+++ src/malone/handler.clj
@@ -3,12 +3,43 @@
             [ring.util.http-response :refer :all]
             [schema.core :as s]))
 
-(s/defschema Pizza
-  {:name s/Str
-   (s/optional-key :description) s/Str
-   :size (s/enum :L :M :S)
-   :origin {:country (s/enum :FI :PO)
-            :city s/Str}})
+(s/defschema IteneraryItem
+  {:title s/Str
+   :description s/Str
+   :distance s/Num
+   :event_time s/Num
+   :location {:address s/Str
+              :lat s/Num
+              :long s/Num}})
+
+(s/defschema IteneraryRequest
+  {:start_time s/Num
+   :end_time s/Num
+   :start_point s/Str})
+
+(defn get-itenerary [start-time end-time start-point]
+  (println (str "Received " start-time end-time start-point))
+  [{:title "First title"
+                  :description "First description"
+                  :event_time 1487983960
+                  :distance 1.0
+                  :location {:address "First address"
+                             :lat 1111.11111
+                             :long -1111.11111}}
+   {:title "Second title"
+                  :description "Second description"
+                  :event_time 1487983960
+                  :distance 2.0
+                  :location {:address "Second address"
+                             :lat 2222.22222
+                             :long -2222.22222}}
+   {:title "Third title"
+                  :description "Third description"
+                  :event_time 1487983960
+                  :distance 3.0
+                  :location {:address "Third address"
+                             :lat 3333.33333
+                             :long -3333.33333}}])
 
 (def app
   (api
@@ -16,20 +47,14 @@
      {:ui "/"
       :spec "/swagger.json"
       :data {:info {:title "Malone"
-                    :description "Compojure Api example"}
-             :tags [{:name "api", :description "some apis"}]}}}
+                    :description "Malone API"}
+             :tags [{:name "api", :description "Malone APIs"}]}}}
 
     (context "/api" []
       :tags ["api"]
 
-      (GET "/plus" []
-        :return {:result Long}
-        :query-params [x :- Long, y :- Long]
-        :summary "adds two numbers together"
-        (ok {:result (+ x y)}))
-
-      (POST "/echo" []
-        :return Pizza
-        :body [pizza Pizza]
-        :summary "echoes a Pizza"
-        (ok pizza)))))
+      (POST "/itenerary" []
+        :return [IteneraryItem]
+        :body [itenerary_request IteneraryRequest]
+        :summary "Generates an itenerary for a given start/end time and initial location"
+        (ok (get-itenerary 1 1 "test"))))))
